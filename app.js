635a92cf543fdd42b4db8b61c75b74eda344b87e
Nimur Rhaman Nerob <71459304+nimurr@users.noreply.github.com>
2022-08-07 03:27:50 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+let nav = document.querySelector('ul');
+let bars = document.querySelector('.mobile-manu');
+
+
+bars.addEventListener("click", ()=>{
+    // bars.classList.toggle('fa-times');
+    nav.classList.toggle('close')
+  })
+
